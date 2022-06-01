a22eed0480ce3d0eb6d263348e8a0681164aa46d
Ikramova Iroda <ikromovabdumajid2007@gmail.com>
2022-06-01 11:53:52 +0300

    Deploy to heroku

--- src/main/java/com/company/rayxon/controller/ProductController.java
+++ src/main/java/com/company/rayxon/controller/ProductController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:8080", "http://localhost:3000"}, maxAge = 4800, allowCredentials = "false")
 @RestController
 public class ProductController {
 
